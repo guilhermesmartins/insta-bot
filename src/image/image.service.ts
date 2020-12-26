fdd62a3878d5b29679a39a29cfbf206210eba428
Mitaka Rupau <mitakarupau@pop-os.localdomain>
2020-12-26 18:24:18 -0300

    preparing for updates

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,30 +1,43 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ReceiveAndEditImage } from './dto/receive-edit-image.dto';
-import gm from 'gm';
+import gm, { Dimensions } from 'gm';
 import { join } from 'path';
 import { ReceiveInfoAndInsertInImage } from './dto/receive-info-insert-image.dto';
+import wrap from 'word-wrap';
+import sizeOf from 'image-size';
 
 @Injectable()
 export class ImageService {
   receiveAndEdit(file: ReceiveAndEditImage, info: ReceiveInfoAndInsertInImage) {
     const { path, filename } = file;
-    const { author, text } = info;
+    let { text } = info;
 
     //if(path) image without type in the name crashes
 
-    gm.subClass({ imageMagick: true });
-    //gm(path)
-    //  .resize(240, 240)
-    //  .write(join(__dirname, '..', '..', 'uploads', filename), (err) => {
-    //    if (err) throw new InternalServerErrorException(err);
-    //  });
+    text = `"${text}"`;
 
+    const wrappedText = wrap(text.toUpperCase(), {
+      width: 30,
+      trim: true,
+    });
+
+    const dimensions = sizeOf(path);
+
+    if (dimensions.width < dimensions.height) {
+      const tempValue = dimensions.height;
+    }
+
+    gm.subClass({ imageMagick: true });
     gm(path)
-      .stroke('#000000')
+      .stroke('black')
       .resize(500, 500)
-      .blur(3, 2)
-      .font('Arial.tff', 18)
-      .label(text)
+      .out('-blur', '0x3')
+      .fill('white')
+      //.extent(500, 500)
+      //.blur(2, 1)
+      .font('OpenSans.ttf', 20)
+      .drawText(dimensions.width / 500, dimensions.height / 7, wrappedText)
+      .gravity('SouthWest')
       .write(join(__dirname, '..', '..', 'uploads', filename), (err) => {
         if (err) throw new InternalServerErrorException(err);
       });
